2fc44253074b48f5bb6fd2a0d139eccea94351ab
Lennert De Ryck <lennyderyck@gmail.com>
2025-12-03 20:17:34 +0100

    Bugfix

--- client/app/badge/[...packageNameSlug]/route.tsx
+++ client/app/badge/[...packageNameSlug]/route.tsx
@@ -28,7 +28,7 @@ export const GET = async (request: NextRequest, { params }: { params: Promise<{ 
   const svg = await satori(
     <div style={{ display: "flex", alignItems: 'center', paddingRight: 30, fontFamily: 'Roboto',  fontWeight: 400, color: 'white', backgroundColor: '#010023', borderRadius: 9999 }}>
       <div style={{ display: "flex", alignItems: 'center', paddingTop: 10, paddingBottom: 10}}>
-      <svg 
+      {!isSafe && <svg 
         xmlns="http://www.w3.org/2000/svg" 
         viewBox="0 0 24 24" 
         width={32}
@@ -36,14 +36,19 @@ export const GET = async (request: NextRequest, { params }: { params: Promise<{ 
         fill="currentColor"
         style={{ marginLeft: 20, marginRight: 10 }}
       >
-        {!isSafe ? 
-          <>
-            <path fill="none" d="M0 0h24v24H0z"></path><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path>
-          </>:
-          <><path fill="none" d="M0 0h24v24H0z"></path><path d="M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z"></path>
-          </>
-        }
-      </svg>
+        <path fill="none" d="M0 0h24v24H0z"></path><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path>
+      </svg>}
+      {isSafe && <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 24 24" 
+        width={32}
+        height={32}
+        fill="currentColor"
+        style={{ marginLeft: 20, marginRight: 10 }}
+      >
+        <path fill="none" d="M0 0h24v24H0z"></path><path d="M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z"></path>
+        
+      </svg>}
         <div style={{ display: "flex", alignItems: 'center', fontFamily: 'Albert Sans', fontWeight: 500, fontSize: 32 }}>
           Package is {hasResult ? 'malicious' : 'safe'}
         </div>
